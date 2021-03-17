4e128596714f2d4dcd1fee74e17bc81d8307c994
ValerioSchips <69105970+ValerioSchips@users.noreply.github.com>
2021-03-17 14:34:35 +0100

    Add files via upload

---
+++ NOServConnectFramework.framework/Versions/A/Headers/NOServConnectFramework.h
@@ -0,0 +1,18 @@
+//
+//  NOServConnectFramework.h
+//  NOServConnectFramework
+//
+//  Created by Valerio Schips on 25/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NOServConnectFramework.
+FOUNDATION_EXPORT double NOServConnectFrameworkVersionNumber;
+
+//! Project version string for NOServConnectFramework.
+FOUNDATION_EXPORT const unsigned char NOServConnectFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NOServConnectFramework/PublicHeader.h>
+
+
