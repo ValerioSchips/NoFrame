e02f156ba1ebb726855237e4c618508bb04279b2
ValerioSchips <valerio_schips@hotmail.it>
2021-03-18 18:32:58 +0100

    FrameUpdate

---
+++ NOServConnectFrameworkIOS.framework/Headers/NOServConnectFrameworkIOS.h
@@ -0,0 +1,18 @@
+//
+//  NOServConnectFrameworkIOS.h
+//  NOServConnectFrameworkIOS
+//
+//  Created by Valerio Schips on 18/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NOServConnectFrameworkIOS.
+FOUNDATION_EXPORT double NOServConnectFrameworkIOSVersionNumber;
+
+//! Project version string for NOServConnectFrameworkIOS.
+FOUNDATION_EXPORT const unsigned char NOServConnectFrameworkIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NOServConnectFrameworkIOS/PublicHeader.h>
+
+
